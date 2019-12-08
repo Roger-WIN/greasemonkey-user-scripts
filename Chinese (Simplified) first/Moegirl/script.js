fa46090667750cdb79df24dd33f470bb3a36e818
Roger Kung .WIN <RogerKung.WIN@outlook.com>
2019-12-08 12:15:45 +0800

    Fit language-first v1.0.3

--- Chinese (Simplified) first/Moegirl/script.js
+++ Chinese (Simplified) first/Moegirl/script.js
@@ -2,9 +2,10 @@
 // @name             萌娘百科优先简体中文
 // @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
 // @description      Moegirl 萌娘百科优先使用简体中文浏览
-// @version          1.1.2
-// @match            *.moegirl.org/*
-// @require          https://greasyfork.org/scripts/392621-%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87/code/%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.js?version=750875
+// @version          1.1.3
+// @match            *zh.moegirl.org/*
+// @match            *mzh.moegirl.org/*
+// @require          https://greasyfork.org/scripts/392621-%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87/code/%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.js?version=756138
 // @author           神齐 <RogerKung.WIN@outlook.com>
 // @license          MIT
 // @updateURL        https://raw.githubusercontent.com/Roger-WIN/greasemonkey-user-scripts/master/Chinese%20(Simplified)%20first/Moegirl/script.js
@@ -17,7 +18,7 @@
 
     var lang_target = "/zh-cn/"; // 目标语言
     var langs_head = "/zh"; // 表示页面是否指定语言
-    var flag_exclude = "."; // 表示路径中含有文件，因为文件的扩展名以 . 开头
+    var flags_exclude = ['.']; // 表示路径中含有文件，因为文件的扩展名以 . 开头
 
-    convertWithExclude(lang_target, langs_head, flag_exclude);
+    convertWithExclude(lang_target, langs_head, flags_exclude);
 })();
--- Chinese (Simplified) first/Wikipedia (Chinese)/script.js
+++ Chinese (Simplified) first/Wikipedia (Chinese)/script.js
@@ -2,10 +2,10 @@
 // @name             中文维基百科优先简体中文
 // @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
 // @description      Wikipedia 维基百科（中文）优先使用简体中文浏览
-// @version          1.1.3
+// @version          1.1.4
 // @match            *zh.wikipedia.org/*
 // @match            *zh.m.wikipedia.org/*
-// @require          https://greasyfork.org/scripts/392621-%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87/code/%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.js?version=750875
+// @require          https://greasyfork.org/scripts/392621-%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87/code/%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.js?version=756138
 // @author           神齐 <RogerKung.WIN@outlook.com>
 // @license          MIT
 // @updateURL        https://raw.githubusercontent.com/Roger-WIN/greasemonkey-user-scripts/master/Chinese%20(Simplified)%20first/Wikipedia%20(Chinese)/script.js
@@ -19,7 +19,7 @@
     var lang_target = "/zh-cn/"; // 目标语言
     var langs_head = "/zh"; // 表示页面是否指定语言
     var lang_default = "/wiki/";
-    var flag_exclude = "/w/";
+    var flags_exclude = ["/w/"];
 
-    convertWithExclude(lang_target, langs_head, flag_exclude, lang_default);
+    convertWithExclude(lang_target, langs_head, flags_exclude, lang_default);
 })();
