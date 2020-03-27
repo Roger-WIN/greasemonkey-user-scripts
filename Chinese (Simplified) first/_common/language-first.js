8bef29257ae10f9eccd58dc61ddfed15535cc465
Roger Kung .WIN <RogerKung.WIN@outlook.com>
2020-03-27 18:15:36 +0800

    Fix bug due to function overloading

--- Chinese (Simplified) first/_common/language-first.js
+++ Chinese (Simplified) first/_common/language-first.js
@@ -2,7 +2,7 @@
 // @name             优先简体中文
 // @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
 // @description      网站优先使用简体中文浏览
-// @version          1.0.4
+// @version          1.0.5
 // @author           神齐 <RogerKung.WIN@outlook.com>
 // @license          MIT
 // @updateURL        https://github.com/Roger-WIN/greasemonkey-user-scripts/raw/master/Chinese%20(Simplified)%20first/_common/language-first.js
@@ -18,41 +18,25 @@ const url = window.location.href,
     pathName = window.location.pathname,
     fullPath = url.substring(home.length);
 
-function convert(lang_target) {
-    let index = lang_target.length;
-    let newUrl = home.concat(lang_target, fullPath.substring(index)); // 替换或指定为目标语言
-    window.location.replace(newUrl); // 替换网页
-}
-
 function convert(lang_target, langs_head, lang_default) {
-    let index = pathName.startsWith(langs_head) ? (fullPath.substring(1).indexOf('/') + 2) : lang_default.length;
+    let index = (arguments.length > 1) ?
+        (pathName.startsWith(langs_head) ?
+            (fullPath.substring(1).indexOf('/') + 2) :
+            lang_default.length
+        ) :
+        lang_target.length;
     let newUrl = home.concat(lang_target, fullPath.substring(index)); // 替换或指定为目标语言
     window.location.replace(newUrl); // 替换网页
-}
-
-/* 没有需要排除的情况 */
-function convertWithoutExclude(lang_target) {
-    if (pathName.startsWith(lang_target)) // 网页已转换
-        return; // 退出函数，避免重复转换
-    convert(lang_target);
 }
 
 /* 没有需要排除的情况 */
 function convertWithoutExclude(lang_target, langs_head, lang_default = '/') {
     if (pathName.startsWith(lang_target)) // 网页已转换
         return; // 退出函数，避免重复转换
-    convert(lang_target, langs_head, lang_default);
-}
-
-/* 有需要排除的情况 */
-function convertWithExclude(lang_target, flags_exclude) {
-    if (pathName.startsWith(lang_target)) // 网页已转换
-        return; // 退出函数，避免重复转换
-    flags_exclude.forEach(flagItem => {
-        if (pathName.includes(flagItem)) // 网页不可转换
-            return; // 退出函数，避免转换
-    });
-    convert(lang_target);
+    if (arguments.length > 1)
+        convert(lang_target, langs_head, lang_default);
+    else
+        convert(lang_target);
 }
 
 /* 有需要排除的情况 */
@@ -63,5 +47,8 @@ function convertWithExclude(lang_target, langs_head, flags_exclude, lang_default
         if (pathName.includes(flagItem)) // 网页不可转换
             return; // 退出函数，避免转换
     });
-    convert(lang_target, langs_head, lang_default);
+    if (arguments.length > 2)
+        convert(lang_target, langs_head, lang_default);
+    else
+        convert(lang_target);
 }
