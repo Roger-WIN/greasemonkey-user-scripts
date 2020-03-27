a0d1c51eeeab98f0cd7e13595893b11938583b6c
Roger Kung .WIN <RogerKung.WIN@outlook.com>
2020-03-27 17:00:28 +0800

    Create scripts for Microsoft Docs and MDN Web Docs

---
+++ Chinese (Simplified) first/MDN Web Docs/script.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name             MDN Web 文档优先简体中文
+// @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
+// @description      MDN Web Docs（MDN Web 文档）优先使用简体中文浏览
+// @version          1.0.0
+// @match            *developer.mozilla.org/*
+// @require          https://greasyfork.org/scripts/392621-%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87/code/%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.js
+// @author           神齐 <RogerKung.WIN@outlook.com>
+// @license          MIT
+// @updateURL        https://github.com/Roger-WIN/greasemonkey-user-scripts/raw/master/Chinese%20(Simplified)%20first/MDN%20Web%20Docs/script.js
+// @supportURL       https://github.com/Roger-WIN/greasemonkey-user-scripts/issues
+// @contributionURL  https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=RogerKung.WIN@outlook.com&item_name=Greasy+Fork+donation
+// ==/UserScript==
+
+/* jshint esversion: 6 */
+
+(function () {
+    const lang_target = "/zh-CN/"; // 目标语言
+    convertWithoutExclude(lang_target);
+})();
---
+++ Chinese (Simplified) first/Microsoft Docs/script.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name             微软文档优先简体中文
+// @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
+// @description      Microsoft Docs 微软文档优先使用简体中文浏览
+// @version          1.0.0
+// @match            *docs.microsoft.com/*
+// @require          https://greasyfork.org/scripts/392621-%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87/code/%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.js
+// @author           神齐 <RogerKung.WIN@outlook.com>
+// @license          MIT
+// @updateURL        https://github.com/Roger-WIN/greasemonkey-user-scripts/raw/master/Chinese%20(Simplified)%20first/Microsoft%20Docs/script.js
+// @supportURL       https://github.com/Roger-WIN/greasemonkey-user-scripts/issues
+// @contributionURL  https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=RogerKung.WIN@outlook.com&item_name=Greasy+Fork+donation
+// ==/UserScript==
+
+/* jshint esversion: 6 */
+
+(function () {
+    const lang_target = "/zh-cn/"; // 目标语言
+    convertWithoutExclude(lang_target);
+})();
