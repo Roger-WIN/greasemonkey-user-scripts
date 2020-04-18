e6e78766d0de33386a0588124c39c1f6e7090cd3
Roger Kung .WIN <RogerKung.WIN@outlook.com>
2020-04-18 12:17:20 +0800

    Set the adaptation range

--- Chinese (Simplified) first/_common/language-first.js
+++ Chinese (Simplified) first/_common/language-first.js
@@ -2,7 +2,8 @@
 // @name             优先简体中文
 // @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
 // @description      网站优先使用简体中文浏览
-// @version          1.0.8
+// @version          1.0.9
+// @match            *
 // @author           神齐 <RogerKung.WIN@outlook.com>
 // @license          MIT
 // @updateURL        https://github.com/Roger-WIN/greasemonkey-user-scripts/raw/master/Chinese%20(Simplified)%20first/_common/language-first.js
