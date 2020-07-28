d2d009e5ca65a8ccf7994cfa8940f8699b2248b3
Roger Kung .WIN <RogerKung.WIN@outlook.com>
2020-07-28 21:24:50 +0800

    fit more domain

--- Chinese (Simplified) first/Moegirl/script.js
+++ Chinese (Simplified) first/Moegirl/script.js
@@ -2,9 +2,11 @@
 // @name             萌娘百科优先简体中文
 // @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
 // @description      Moegirl 萌娘百科优先使用简体中文浏览
-// @version          1.1.7
+// @version          1.1.8
 // @match            *zh.moegirl.org/*
 // @match            *mzh.moegirl.org/*
+// @match            *zh.moegirl.org.cn/*
+// @match            *mzh.moegirl.org.cn/*
 // @require          https://greasyfork.org/scripts/392621-%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87/code/%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.js
 // @author           神齐 <RogerKung.WIN@outlook.com>
 // @license          MIT
@@ -16,6 +18,6 @@
 (() => {
     const lang_target = "/zh-cn/"; // 目标语言
     let langs_head = "/zh"; // 表示页面是否指定语言
-    let flags_exclude = ["index.php", '.']; // 表示路径中含有文件，因为文件的扩展名以 . 开头
+    let flags_exclude = ["index.php", "."]; // 表示路径中含有文件，因为文件的扩展名以 . 开头
     convertWithExclude(lang_target, flags_exclude, langs_head);
 })();
