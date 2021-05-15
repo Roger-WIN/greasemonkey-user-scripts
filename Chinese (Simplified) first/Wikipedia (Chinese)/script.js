597a620e7abc504d6b4d0a6e38333cddf371ac18
Roger Kung .WIN <RogerKung.WIN@outlook.com>
2021-05-16 00:44:59 +0800

    Add initial support for Cantonese

--- Chinese (Simplified) first/Wikipedia (Chinese)/script.js
+++ Chinese (Simplified) first/Wikipedia (Chinese)/script.js
@@ -2,9 +2,11 @@
 // @name             中文维基百科优先简体中文
 // @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
 // @description      Wikipedia 维基百科（中文）优先使用简体中文浏览
-// @version          1.1.10
+// @version          1.2.0
 // @match            *zh.wikipedia.org/*
+// @match            *zh-yue.wikipedia.org/*
 // @match            *zh.m.wikipedia.org/*
+// @match            *zh-yue.m.wikipedia.org/*
 // @require          https://greasyfork.org/scripts/392621-%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87/code/%E4%BC%98%E5%85%88%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.js
 // @author           神齐 <RogerKung.WIN@outlook.com>
 // @license          MIT
@@ -20,5 +22,7 @@
     let langs_head = "/zh";
     let lang_default = "/wiki/";
     let flags_exclude = ["index.php", "/w/"];
-    convertWithExclude(lang_target, flags_exclude, langs_head, lang_default);
+    if (convertSubdomain()) {
+        convertWithExclude(lang_target, flags_exclude, langs_head, lang_default);
+    }
 })();
--- Chinese (Simplified) first/_common/language-first.js
+++ Chinese (Simplified) first/_common/language-first.js
@@ -2,7 +2,7 @@
 // @name             优先简体中文
 // @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
 // @description      网站优先使用简体中文浏览
-// @version          1.0.11
+// @version          1.1.0
 // @match            *
 // @author           神齐 <RogerKung.WIN@outlook.com>
 // @license          MIT
@@ -12,8 +12,9 @@
 // ==/UserScript==
 
 const url = window.location.href,
-    domain = window.location.hostname,
-    home = window.location.protocol.concat("//", domain),
+    domain = window.location.host,
+    protocol = window.location.protocol + "//",
+    home = protocol + domain,
     pathName = window.location.pathname,
     fullPath = url.substring(home.length);
 
@@ -25,10 +26,10 @@ const convert = (lang_target, ...args) => {
             fullPath.substring(1).indexOf("/") + 2 :
             lang_default.length;
     } else {
-        index = lang_target.length
+        index = lang_target.length;
     }
     // 替换或指定为目标语言
-    let newUrl = home.concat(lang_target, fullPath.substring(index));
+    let newUrl = home + lang_target + fullPath.substring(index);
     // 替换网页
     window.location.replace(newUrl);
 };
@@ -68,4 +69,39 @@ const convertWithExclude = (lang_target, flags_exclude, ...args) => {
     } else {
         convert(lang_target);
     }
+};
+
+const convertSubdomain = (subdomain_target = "zh", subdomain_preserve = 'm') => {
+    const DELIMITER = '.';
+    const domains = domain.split(DELIMITER),
+        mainDomains = domains.slice(-2),
+        subdomains = domains.slice(0, -2),
+        mainDomain = mainDomains.join(DELIMITER),
+        subdomain = subdomains.join(DELIMITER);
+
+    const isMultiSubdomain = subdomains.length > 1;
+
+    // 域名中不包含欲保留的子域名时，不保留
+    let isPreserve = (subdomain_preserve.length > 0) && (subdomain.startsWith(subdomain_preserve) != subdomain.endsWith(subdomain_preserve));
+
+    if (isMultiSubdomain && isPreserve && subdomains.includes(subdomain_target)) {
+        return true;
+    }
+    if (!isMultiSubdomain) {
+        if (subdomain === subdomain_target) {
+            return true;
+        }
+        if (isPreserve && [subdomain_preserve + subdomain_target, subdomain_target + subdomain_preserve].includes(subdomain)) {
+            return true;
+        }
+    }
+
+    let new_subdomain = subdomain_target;
+    if (isPreserve) {
+        let separator = isMultiSubdomain ? DELIMITER : '';
+        new_subdomain = subdomain.startsWith(subdomain_preserve) ? [subdomain_preserve, subdomain_target].join(separator) : [subdomain_target, subdomain_preserve].join(separator);
+    }
+    let new_domain = new_subdomain + DELIMITER + mainDomain;
+    let newUrl = protocol + new_domain + fullPath;
+    window.location.replace(newUrl);
 };
