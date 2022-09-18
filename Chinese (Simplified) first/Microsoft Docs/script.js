c764549f5a2640ff25c7858503c70a6b347f4e49
Roger Kung .WIN <RogerKung.WIN@outlook.com>
2022-09-18 16:47:14 +0800

    perf: 添加对微软文档新域名 learn.microsoft.com 的支持

--- Chinese (Simplified) first/Microsoft Docs/script.js
+++ Chinese (Simplified) first/Microsoft Docs/script.js
@@ -2,9 +2,10 @@
 // @name             微软文档优先简体中文
 // @namespace        https://github.com/Roger-WIN/greasemonkey-user-scripts
 // @description      Microsoft Docs 微软文档优先使用简体中文浏览
-// @version          1.0.5
+// @version          1.0.6
 // @match            *docs.microsoft.com/*
 // @match            *support.microsoft.com/*
+// @match            *learn.microsoft.com/*
 // @require          https://cdn.jsdelivr.net/gh/Roger-WIN/greasemonkey-user-scripts@a44e2ee0a802fd0fbed6c461196188237294fc32/Chinese%20(Simplified)%20first/_common/language-first.js
 // @author           神齐 <RogerKung.WIN@outlook.com>
 // @license          MIT
